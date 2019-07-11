5484a22e356e5dfc95a1033f938995ccd1b4f367
Michael Auer <auermich@fim.uni-passau.de>
2019-07-11 13:15:34 +0200

    refactored vertex representation

--- src/main/java/de/uni_passau/fim/auermich/graphs/Vertex.java
+++ src/main/java/de/uni_passau/fim/auermich/graphs/Vertex.java
@@ -19,32 +19,31 @@ public class Vertex {
     // uniquely identifies each vertex by its method signature
     private final String method;
 
+    private final VERTEX_TYPE type;
+
     public Vertex(int id, AnalyzedInstruction instruction, String method) {
         this.id = id;
         this.instruction = instruction;
         this.method = method;
+        type = VERTEX_TYPE.mapType(id);
     }
 
     public String toString() {
 
-        if (isEntryVertex()) {
-            return "entry " + " (" + method + ")";
-        } else if (isExitVertex()) {
-            return "exit " + " (" + method + ")";
-        } else if (isReturnVertex()) {
-            return "return";
+        if (isEntryVertex() || isExitVertex() || isReturnVertex()) {
+            return type + " (" + method + ")";
         } else {
-            return id + ": " + instruction.getInstruction().getOpcode().name + " (" + method + ")";
+            return String.valueOf(id); // + ": " + instruction.getInstruction().getOpcode().name;
         }
     }
 
     public boolean isEntryVertex() {
-        return id == -1;
+        return type == VERTEX_TYPE.ENTRY_VERTEX;
     }
 
-    public boolean isExitVertex() { return id == -2; }
+    public boolean isExitVertex() { return type == VERTEX_TYPE.EXIT_VERTEX; }
 
-    public boolean isReturnVertex() { return id == -3; }
+    public boolean isReturnVertex() { return type == VERTEX_TYPE.RETURN_VERTEX; }
 
     public AnalyzedInstruction getInstruction() {
         return instruction;
@@ -70,6 +69,49 @@ public class Vertex {
     @Override
     public int hashCode() {
         return Objects.hash(method, id);
+    }
+
+
+    /**
+     * Represents the type of vertex. This can be either
+     * an entry vertex, an exit vertex, a return vertex or
+     * simply a normal vertex.
+     */
+    private enum VERTEX_TYPE {
+
+        ENTRY_VERTEX,
+        EXIT_VERTEX,
+        RETURN_VERTEX,
+        NORMAL_VERTEX;
+
+        /**
+         * Maps a given number to its vertex type.
+         * @param num The number representing a vertex type.
+         * @return Returns the vertex type corresponding to the given number.
+         */
+        private static VERTEX_TYPE mapType(int num) {
+            if (num == -1 ) {
+                return ENTRY_VERTEX;
+            } else if (num == -2 ) {
+                return EXIT_VERTEX;
+            } else if (num == -3 ) {
+                return RETURN_VERTEX;
+            } else {
+                return NORMAL_VERTEX;
+            }
+        }
+
+        @Override
+        public String toString() {
+            switch (this) {
+                case EXIT_VERTEX: return "exit";
+                case ENTRY_VERTEX: return "entry";
+                case RETURN_VERTEX: return "return";
+                case NORMAL_VERTEX: return "";
+            }
+            throw new UnsupportedOperationException();
+        }
+
     }
 
 }
