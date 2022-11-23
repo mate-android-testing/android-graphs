75936f680dcbfd0910a029d76af273c240422b39
Michael <auermich@fim.uni-passau.de>
2022-11-23 20:56:44 +0100

    added convenience function to construct call tree via mate server

--- android-graphs-core/src/main/java/de/uni_passau/fim/auermich/android_graphs/core/utility/GraphUtils.java
+++ android-graphs-core/src/main/java/de/uni_passau/fim/auermich/android_graphs/core/utility/GraphUtils.java
@@ -3,6 +3,7 @@ package de.uni_passau.fim.auermich.android_graphs.core.utility;
 import de.uni_passau.fim.auermich.android_graphs.core.graphs.BaseGraph;
 import de.uni_passau.fim.auermich.android_graphs.core.graphs.BaseGraphBuilder;
 import de.uni_passau.fim.auermich.android_graphs.core.graphs.GraphType;
+import de.uni_passau.fim.auermich.android_graphs.core.graphs.calltree.CallTree;
 import de.uni_passau.fim.auermich.android_graphs.core.graphs.cfg.BaseCFG;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -156,5 +157,66 @@ public class GraphUtils {
         LOGGER.info("Graph construction took: " + ((end - start) / 1000) + " seconds");
 
         return (BaseCFG) baseGraph;
+    }
+
+    /**
+     * Convenient function to construct a call tree. Should be used for the construction requested by mate server.
+     *
+     * @param apkPath           The path to the APK file.
+     * @param excludeARTClasses Whether to exclude ART classes or not.
+     * @param onlyResolveAUTClasses Whether only AUT classes should be resolved.
+     * @return Returns a call tree.
+     */
+    public static CallTree constructCallTree(final File apkPath, final boolean excludeARTClasses,
+                                             final boolean onlyResolveAUTClasses) {
+
+        LOGGER.info("Constructing CALL TREE for APK: " + apkPath);
+
+        long start = System.currentTimeMillis();
+
+        MultiDexContainer<? extends DexBackedDexFile> apk = null;
+
+        try {
+            apk = DexFileFactory.loadDexContainer(apkPath, Utility.API_OPCODE);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+        List<DexFile> dexFiles = new ArrayList<>();
+        List<String> dexEntries = new ArrayList<>();
+
+        try {
+            dexEntries = apk.getDexEntryNames();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+        for (String dexEntry : dexEntries) {
+            try {
+                dexFiles.add(apk.getEntry(dexEntry).getDexFile());
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+
+        BaseGraphBuilder builder = new BaseGraphBuilder(GraphType.CALLTREE, dexFiles)
+                .withName("global")
+                .withBasicBlocks()
+                .withAPKFile(apkPath);
+
+        if (excludeARTClasses) {
+            builder = builder.withExcludeARTClasses();
+        }
+
+        if (onlyResolveAUTClasses) {
+            builder = builder.withResolveOnlyAUTClasses();
+        }
+
+        BaseGraph baseGraph = builder.build();
+
+        long end = System.currentTimeMillis();
+        LOGGER.info("Graph construction took: " + ((end - start) / 1000) + " seconds");
+
+        return (CallTree) baseGraph;
     }
 }
