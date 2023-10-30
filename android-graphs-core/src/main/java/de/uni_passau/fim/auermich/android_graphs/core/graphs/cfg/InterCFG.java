48921509e03ba9799f602ecffbddd9227c5f0fd0
Michael <auermich@fim.uni-passau.de>
2023-10-30 17:36:10 +0100

    minor code layout fix

--- android-graphs-core/src/main/java/de/uni_passau/fim/auermich/android_graphs/core/graphs/cfg/InterCFG.java
+++ android-graphs-core/src/main/java/de/uni_passau/fim/auermich/android_graphs/core/graphs/cfg/InterCFG.java
@@ -330,11 +330,11 @@ public class InterCFG extends BaseCFG {
      */
     private Set<BaseCFG> lookupTargetCFGs(final APK apk, final BasicStatement invokeStmt) {
 
-        Set<BaseCFG> targetCFGs = new HashSet<>();
+        final Set<BaseCFG> targetCFGs = new HashSet<>();
 
-        Instruction instruction = invokeStmt.getInstruction().getInstruction();
-        String targetMethod = ((ReferenceInstruction) instruction).getReference().toString();
-        String callingClass = MethodUtils.getClassName(invokeStmt.getMethod());
+        final Instruction instruction = invokeStmt.getInstruction().getInstruction();
+        final String targetMethod = ((ReferenceInstruction) instruction).getReference().toString();
+        final String callingClass = MethodUtils.getClassName(invokeStmt.getMethod());
 
         LOGGER.debug("Lookup target CFGs for " + targetMethod);
 
@@ -343,7 +343,7 @@ public class InterCFG extends BaseCFG {
          * that overrides the given method. Thus, we need to over-approximate in this case
          * and connect the invoke with each overridden method (CFG) as well.
          */
-        Set<String> overriddenMethods = classHierarchy.getOverriddenMethods(callingClass, targetMethod,
+        final Set<String> overriddenMethods = classHierarchy.getOverriddenMethods(callingClass, targetMethod,
                 apk.getManifest().getPackageName(), properties);
 
         for (String overriddenMethod : overriddenMethods) {
